2ee1cd624c87ae34fc24099d2c3fab91d9d9bc3d
Erick Christian <erickchristian48@gmail.com>
2025-02-14 16:59:54 +0700

    test: Add create and expose tests

---
+++ test/node/create.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest"
+
+describe("create", () => {
+  it("should create a new worker instance and set up its communication channels correctly", () => {
+    expect(true).toBeTruthy()
+  })
+})
---
+++ test/node/expose.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest"
+
+describe("expose", () => {
+  it("should call the provided function with the payload and post back a valid response", () => {
+    expect(true).toBeTruthy()
+  })
+})
