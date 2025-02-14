ed874ee9cbbb8388d2ce315846c7bc485f38668a
Erick Christian <erickchristian48@gmail.com>
2025-02-14 19:22:46 +0700

    chore: Remove obsolete expose test file

--- test/node/expose.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest"
-
-describe("expose", () => {
-  it("should call the provided function with the payload and post back a valid response", () => {
-    expect(true).toBeTruthy()
-  })
-})
